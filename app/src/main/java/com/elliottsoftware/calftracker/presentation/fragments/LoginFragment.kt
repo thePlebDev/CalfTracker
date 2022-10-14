7bfb85009c948211b7ff9c1bb633cadcf72b768e
Tristan Elliott <>
2022-10-14 14:06:34 -0300

    moving login screen to compose View

--- app/src/main/java/com/elliottsoftware/calftracker/presentation/fragments/LoginFragment.kt
+++ app/src/main/java/com/elliottsoftware/calftracker/presentation/fragments/LoginFragment.kt
@@ -5,6 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.elliottsoftware.calftracker.R
 import com.elliottsoftware.calftracker.databinding.FragmentLoginBinding
 
@@ -32,13 +46,44 @@ class LoginFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentLoginBinding.inflate(inflater,container,false)
         val view = binding.root
+        binding.composeView.apply {
+            // Dispose of the Composition when the view's LifecycleOwner
+            // is destroyed
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+
+                Column(modifier = Modifier.fillMaxSize(),horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text("Calf Tracker", fontSize = 40.sp
+                        ,fontWeight = FontWeight.Bold,textAlign = TextAlign.Center,
+                        fontFamily = FontFamily.SansSerif,modifier = Modifier.padding(top = 16.dp))
+                    Text("Powered by Elliott Software", fontSize = 18.sp
+                        ,fontWeight = FontWeight.Light,textAlign = TextAlign.Center,
+                        fontFamily = FontFamily.SansSerif)
+                    SimpleFilledTextFieldSample()
+
+                }
+
+            }
+
+        }
         return view
     }
-
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    @Composable
+    fun SimpleFilledTextFieldSample() {
+        var text by remember { mutableStateOf("Hello") }
+
+        TextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text("Label",fontSize = 24.sp) },
+            modifier = Modifier.padding(top = 32.dp)
+        )
     }
 
 
