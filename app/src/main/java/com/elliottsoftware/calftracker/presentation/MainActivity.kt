aa9f87b0e4aae1147963564e4080014ee34b0ff2
Tristan Elliott <>
2022-10-14 12:07:18 -0300

    initial commit

---
+++ app/src/main/java/com/elliottsoftware/calftracker/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.elliottsoftware.calftracker.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.elliottsoftware.calftracker.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
