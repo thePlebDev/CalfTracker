987c502ac4544756d1abfc6a57f6d3431942e337
Tristan Elliott <>
2022-10-15 00:10:32 -0300

    adding password validation compose

--- app/src/main/java/com/elliottsoftware/calftracker/presentation/components/login/LoginView.kt
+++ app/src/main/java/com/elliottsoftware/calftracker/presentation/components/login/LoginView.kt
@@ -1,33 +1,36 @@
 package com.elliottsoftware.calftracker.presentation.components.login
 
+import com.elliottsoftware.calftracker.R.drawable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.R
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.elliottsoftware.calftracker.presentation.viewModels.LoginViewModel
 
+
 @Composable
 fun LoginView(viewModel: LoginViewModel = viewModel()) {
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
         BannerCard("Calf Tracker", "powered by Elliott Software")
         EmailInput(viewModel)
-//        PasswordInput(viewModel,state)
+        PasswordInput(viewModel)
         SubmitButton(viewModel)
 
 
@@ -75,6 +78,45 @@ fun EmailInput(loginViewModel: LoginViewModel){
                 Alignment.End))
         }
 
+    }
+
+}
+@Composable
+fun PasswordInput(viewModel: LoginViewModel){
+    val state = viewModel.state.value
+
+    val icon = if(state.passwordIconChecked)
+        painterResource(id = drawable.design_ic_visibility)
+    else
+        painterResource(id = drawable.design_ic_visibility_off)
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        OutlinedTextField(value = state.password,
+            onValueChange = {viewModel.updatePassword(it)},
+            placeholder = { Text(text = "Password", fontSize = 26.sp) },
+            modifier = Modifier.padding(start = 0.dp, 10.dp, 0.dp, 0.dp),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password
+            ),
+            isError = state.passwordError != null,
+            trailingIcon = {
+                IconButton(onClick = {
+                    viewModel.passwordIconChecked(!state.passwordIconChecked)
+                }) {
+                    Icon(painter = icon, contentDescription = "Visibility Icon")
+                }
+            },
+            visualTransformation = if (state.passwordIconChecked) VisualTransformation.None
+            else PasswordVisualTransformation(),
+            textStyle = TextStyle(fontSize = 26.sp)
+        )
+        if (state.passwordError != null) {
+            Text(
+                text = state.passwordError,
+                color = MaterialTheme.colors.error,
+                modifier = Modifier.align(Alignment.End)
+            )
+        }
     }
 
 }
--- app/src/main/java/com/elliottsoftware/calftracker/presentation/viewModels/LoginViewModel.kt
+++ app/src/main/java/com/elliottsoftware/calftracker/presentation/viewModels/LoginViewModel.kt
@@ -5,7 +5,11 @@ import androidx.lifecycle.ViewModel
 
 data class LoginUIState(
     val email:String = "",
-    val emailError:String? = null
+    val emailError:String? = null,
+    val password:String = "",
+    val passwordError: String? = null,
+    val passwordIconChecked:Boolean = false,
+    val showProgressBar:Boolean = false
 )
 
 class LoginViewModel:ViewModel() {
@@ -17,6 +21,7 @@ class LoginViewModel:ViewModel() {
         state.value = state.value.copy(email=email)
 
     }
+
     private fun validateEmail(email:String):String?{
         if(email.isBlank()){
             return "Email can not be blank"
@@ -26,9 +31,26 @@ class LoginViewModel:ViewModel() {
         }
         return null
     }
+    fun updatePassword(password:String){
+        state.value = state.value.copy(password = password)
+    }
+    private fun validatePassword(password:String):String?{
+        if(password.isBlank()){
+            return "Password can not be blank"
+        }
+        if(password.length < 8){
+            return "Password must be greater than 8 characters"
+        }
+        return null
+    }
+    fun passwordIconChecked(checked:Boolean){
+        state.value = state.value.copy(passwordIconChecked= checked)
+
+    }
     fun submitButton(){
         val verifyEmail = validateEmail(state.value.email)
-        state.value = state.value.copy(emailError = verifyEmail)
+        val verifyPassword = validatePassword(state.value.password)
+        state.value = state.value.copy(emailError = verifyEmail, passwordError = verifyPassword)
     }
 
 }
