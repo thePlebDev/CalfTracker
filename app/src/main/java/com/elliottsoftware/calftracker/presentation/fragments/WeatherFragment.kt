5b0d1e8b50bedcff29648d3c9cfa86a8e4cb2c5d
Tristan Elliott <>
2022-11-15 18:59:35 -0400

    updating weather state

--- app/src/main/java/com/elliottsoftware/calftracker/presentation/fragments/WeatherFragment.kt
+++ app/src/main/java/com/elliottsoftware/calftracker/presentation/fragments/WeatherFragment.kt
@@ -6,28 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.elliottsoftware.calftracker.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
+import com.elliottsoftware.calftracker.databinding.FragmentWeatherBinding
 /**
  * A simple [Fragment] subclass.
  * Use the [WeatherFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
 class WeatherFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding:FragmentWeatherBinding? = null;
+    val binding get() = _binding!!
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -35,26 +27,17 @@ class WeatherFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_weather, container, false)
+        _binding = FragmentWeatherBinding.inflate(inflater,container,false)
+        val view = binding.root
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment WeatherFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            WeatherFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null;
     }
+
+
 }
