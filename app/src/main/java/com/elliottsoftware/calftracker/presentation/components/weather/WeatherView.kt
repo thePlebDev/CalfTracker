c2b8f69a24fa09cfe663e438b8750b1ce7a45f7a
Tristan Elliott <>
2022-11-23 23:41:55 -0400

    adding version 1 for WeatherUI

--- app/src/main/java/com/elliottsoftware/calftracker/presentation/components/weather/WeatherView.kt
+++ app/src/main/java/com/elliottsoftware/calftracker/presentation/components/weather/WeatherView.kt
@@ -3,23 +3,30 @@ package com.elliottsoftware.calftracker.presentation.components.weather
 import android.annotation.SuppressLint
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Column
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.focusable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Logout
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Satellite
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.elliottsoftware.calftracker.R
-import com.elliottsoftware.calftracker.domain.models.Response
-import com.elliottsoftware.calftracker.presentation.components.main.DrawerBody
-import com.elliottsoftware.calftracker.presentation.components.main.DrawerHeader
-import com.elliottsoftware.calftracker.presentation.components.main.FloatingButton
-import com.elliottsoftware.calftracker.presentation.components.main.MenuItem
-import com.elliottsoftware.calftracker.presentation.viewModels.EditCalfViewModel
-import com.elliottsoftware.calftracker.presentation.viewModels.MainViewModel
 import com.elliottsoftware.calftracker.presentation.viewModels.WeatherViewModel
 import kotlinx.coroutines.launch
 
@@ -75,13 +82,96 @@ fun ScaffoldView(viewModel: WeatherViewModel = viewModel()) {
 
 @Composable
 fun WeatherStuff(viewModel: WeatherViewModel){
-    when(val response = viewModel.uiState.value.weatherData){
-        is Response.Loading -> Text("LOADING")
-        is Response.Success -> {
+//    when(val response = viewModel.uiState.value.weatherData){
+//        is Response.Loading -> Text("LOADING")
+//        is Response.Success -> {
+//
+//            HorizontalScrollScreen()
+//        }
+//        is Response.Failure -> Text("FAIL")
+//    }
 
-            Text(response.data.size.toString())
+        HorizontalScrollScreen()
+
+
+
+}
+
+@Composable
+fun HorizontalScrollScreen() {
+    // replace with your items...
+    var bigTap by remember { mutableStateOf(false) }
+
+    Column() {
+        val items = (1..168).map { "Item $it" }
+        // a wrapper to fill the entire screen
+        // BowWithConstraints will provide the maxWidth used below
+        Box(modifier = Modifier
+            .weight(2f)
+            .fillMaxWidth()
+            .background(Color.Blue, shape = RoundedCornerShape(4.dp))
+        ) {
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp)
+                .background(Color.Red, shape = RoundedCornerShape(4.dp))) {
+
+            }
+            
         }
-        is Response.Failure -> Text("FAIL")
+
+        Box(modifier = Modifier
+            .weight(1f)
+            .background(Color.Blue, shape = RoundedCornerShape(4.dp))
+        ) {
+            // LazyRow to display your items horizontally
+            LazyRow(
+                modifier = Modifier,
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                state = rememberLazyListState()
+            ) {
+
+                itemsIndexed(items) { index, item ->
+                    CardShown(item)
+                }
+
+            }
+        }
     }
 
+}
+
+
+@Composable
+fun CardShown(item: String){
+
+    // initialize focus reference to be able to request focus programmatically
+    val focusRequester = remember { FocusRequester() }
+// MutableInteractionSource to track changes of the component's interactions (like "focused")
+    val interactionSource = remember { MutableInteractionSource() }
+    // text below will change when we focus it via button click
+    val isFocused = interactionSource.collectIsFocusedAsState().value
+
+
+
+    val extraPadding by animateDpAsState(
+        if (!isFocused ) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+    Card(
+        modifier = Modifier
+            .focusRequester(focusRequester)
+            .focusable(interactionSource = interactionSource)
+            .height(300.dp)
+            .width(160.dp)
+            .padding(extraPadding.coerceAtLeast(0.dp))
+            .clickable {focusRequester.requestFocus()}
+
+
+    ) {
+        Text(item) // card's content
+    }
 }
