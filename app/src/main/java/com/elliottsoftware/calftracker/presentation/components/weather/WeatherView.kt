4751b8137a29d27b821382332f3a9fc4cc7b0a65
Tristan Elliott <>
2022-11-26 20:01:06 -0400

    finished UI for weather fragment

--- app/src/main/java/com/elliottsoftware/calftracker/presentation/components/weather/WeatherView.kt
+++ app/src/main/java/com/elliottsoftware/calftracker/presentation/components/weather/WeatherView.kt
@@ -2,13 +2,12 @@ package com.elliottsoftware.calftracker.presentation.components.weather
 
 import android.annotation.SuppressLint
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.focusable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.interaction.collectIsPressedAsState
@@ -21,12 +20,20 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.focus.onFocusEvent
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.elliottsoftware.calftracker.R
 import com.elliottsoftware.calftracker.presentation.viewModels.WeatherViewModel
 import kotlinx.coroutines.launch
 
@@ -100,33 +107,55 @@ fun WeatherStuff(viewModel: WeatherViewModel){
 @Composable
 fun HorizontalScrollScreen() {
     // replace with your items...
-    var bigTap by remember { mutableStateOf(false) }
 
-    Column() {
+    Column(modifier = Modifier.background(Color(0xFF102840))) {
         val items = (1..168).map { "Item $it" }
         // a wrapper to fill the entire screen
         // BowWithConstraints will provide the maxWidth used below
-        Box(modifier = Modifier
-            .weight(2f)
-            .fillMaxWidth()
-            .background(Color.Blue, shape = RoundedCornerShape(4.dp))
-        ) {
-            Box(modifier = Modifier
+
+            Card(modifier = Modifier
+                .weight(2f)
                 .fillMaxSize()
                 .padding(10.dp)
-                .background(Color.Red, shape = RoundedCornerShape(4.dp))) {
+                .background(Color(0xFF1B3B5A))) {
+                Column(
+                    modifier = Modifier.fillMaxWidth()
+                        .background(Color(0xFF1B3B5A)),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "Today 18:00",
+                        modifier= Modifier
+                            .padding(10.dp)
+                            .align(Alignment.End),
+                        color = Color.White)
+                    Spacer(modifier=Modifier.height(16.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_cloudy ),
+                        contentDescription = "cloudy icon",
+                        modifier = Modifier.width(200.dp)
+                    )
+                    Spacer(modifier=Modifier.height(16.dp))
+                    Text(
+                        text="12.4 °C",
+                        fontSize=50.sp,
+                        color = Color.White
+                    )
+                }
+
 
             }
             
-        }
+
 
         Box(modifier = Modifier
             .weight(1f)
-            .background(Color.Blue, shape = RoundedCornerShape(4.dp))
+            .background(Color(0xFF102840)),
+            contentAlignment = Alignment.TopEnd
+
         ) {
             // LazyRow to display your items horizontally
+
             LazyRow(
-                modifier = Modifier,
                 horizontalArrangement = Arrangement.SpaceEvenly,
                 state = rememberLazyListState()
             ) {
@@ -141,37 +170,44 @@ fun HorizontalScrollScreen() {
 
 }
 
-
 @Composable
 fun CardShown(item: String){
 
+
     // initialize focus reference to be able to request focus programmatically
     val focusRequester = remember { FocusRequester() }
-// MutableInteractionSource to track changes of the component's interactions (like "focused")
+    // MutableInteractionSource to track changes of the component's interactions (like "focused")
     val interactionSource = remember { MutableInteractionSource() }
     // text below will change when we focus it via button click
-    val isFocused = interactionSource.collectIsFocusedAsState().value
-
+    val isFocused = interactionSource.collectIsFocusedAsState().value //recomp cause
 
 
     val extraPadding by animateDpAsState(
-        if (!isFocused ) 48.dp else 0.dp,
+        targetValue = if (isFocused ) 20.dp else 40.dp,
         animationSpec = spring(
             dampingRatio = Spring.DampingRatioMediumBouncy,
             stiffness = Spring.StiffnessLow
         )
     )
     Card(
+        backgroundColor = Color(0xFF1B3B5A),
         modifier = Modifier
-            .focusRequester(focusRequester)
-            .focusable(interactionSource = interactionSource)
-            .height(300.dp)
-            .width(160.dp)
+            .focusRequester(focusRequester) //register focus changes
+            .focusable(interactionSource = interactionSource) //emit focus events
+            .height(200.dp)
+            .width(180.dp)
+            .background(Color(0xFF102840), shape = RoundedCornerShape(4.dp))
             .padding(extraPadding.coerceAtLeast(0.dp))
-            .clickable {focusRequester.requestFocus()}
+            .clickable { focusRequester.requestFocus() } // makes it all work
+
+
+
 
 
     ) {
-        Text(item) // card's content
+        Box(contentAlignment = Alignment.Center) {
+            Text(item, color = Color.White,textAlign = TextAlign.Center,style = MaterialTheme.typography.h6) // card's content
+        }
     }
+
 }
