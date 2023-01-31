27ed52e88ac5bfc67aa53c1310a075356e422d72
Tristan Elliott <>
2023-01-31 10:38:07 -0400

    readding the proper logging

--- app/src/main/java/com/elliottsoftware/calftracker/logging/CrashAndLog.kt
+++ app/src/main/java/com/elliottsoftware/calftracker/logging/CrashAndLog.kt
@@ -6,21 +6,16 @@ import timber.log.Timber
 
 object CrashAndLog {
     fun setupTimber(){
-//        if(BuildConfig.DEBUG){
-//            Timber.plant(object:Timber.DebugTree(){
-//                override fun createStackElementTag(element: StackTraceElement): String? {
-//                    return "(${element.fileName}:${element.lineNumber})#${element.methodName}"
-//                }
-//            })
-//
-//        }else{
-//            Timber.plant(CrashReportingTree())
-//
-//        }
-        Timber.plant(object:Timber.DebugTree(){
-            override fun createStackElementTag(element: StackTraceElement): String? {
-                return "(${element.fileName}:${element.lineNumber})#${element.methodName}"
-            }
-        })
+        if(BuildConfig.DEBUG){
+            Timber.plant(object:Timber.DebugTree(){
+                override fun createStackElementTag(element: StackTraceElement): String? {
+                    return "(${element.fileName}:${element.lineNumber})#${element.methodName}"
+                }
+            })
+
+        }else{
+            Timber.plant(CrashReportingTree())
+
+        }
     }
 }
