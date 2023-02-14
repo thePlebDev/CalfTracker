165ce60ba0abc1d8330f7b39643c145d9cd5351d
Tristan Elliott <>
2023-02-14 14:28:28 -0400

    refactoring mainView

---
+++ app/src/main/java/com/elliottsoftware/calftracker/presentation/sharedViews/ActionBar.kt
@@ -0,0 +1,33 @@
+package com.elliottsoftware.calftracker.presentation.sharedViews
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ActionBar(
+    modifier: Modifier = Modifier,
+    leftAction: @Composable (BoxScope.() -> Unit) = { },
+    centerAction: @Composable (BoxScope.() -> Unit) = { },
+    rightAction: @Composable (BoxScope.() -> Unit) = { },
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(4.dp)
+            .height(80.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(modifier = Modifier.weight(1f)) {
+            leftAction()
+        }
+        Box(modifier = Modifier.weight(1f)) {
+            centerAction()
+        }
+        Box(modifier = Modifier.weight(1f)) {
+            rightAction()
+        }
+    }
+}
