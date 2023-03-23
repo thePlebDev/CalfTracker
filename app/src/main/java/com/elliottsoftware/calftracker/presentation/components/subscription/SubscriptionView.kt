7bf4690cad7289681a99ea6758e98671b35dc2f0
Tristan Elliott <>
2023-03-23 11:55:25 -0300

    adding more subscription code

--- app/src/main/java/com/elliottsoftware/calftracker/presentation/components/subscription/SubscriptionView.kt
+++ app/src/main/java/com/elliottsoftware/calftracker/presentation/components/subscription/SubscriptionView.kt
@@ -310,7 +310,7 @@ fun SubscribedText(value: BillingUiState){
             Text("LOADING SUBSCRIBED STATE")
         }
         is Response.Success -> {
-            Text(state.data.toString() + "SUBSCRIBED STATE")
+            Text(state.data.toString() + " You are Subscribed")
         }
         is Response.Failure -> {
             Text("FAIL SUBSCRIBED STATE")
@@ -320,122 +320,8 @@ fun SubscribedText(value: BillingUiState){
 }
 
 
-@Composable
-fun FreeCard(clicked:Boolean,setIsClicked:(Boolean)-> Unit,changeTextData:()->Unit){
-    val borderColor = if(!clicked) MaterialTheme.colors.primary else Color.White
-
-        Card(
-            modifier = Modifier
-                .width(160.dp)
-                .clickable {
-                    changeTextData()
-                    setIsClicked(false)
-                },
-            elevation = 5.dp,
-            border = BorderStroke(4.dp, borderColor)
-        ) {
-            Column( modifier = Modifier
-                .padding(15.dp),
-                horizontalAlignment = Alignment.Start
-            ) {
-                Text("$00.00/month",modifier = Modifier.padding(bottom = 10.dp),style = MaterialTheme.typography.h6)
-                Text("- 50 calf limit")
-                Text("- Offline usage")
-                Text("- Cloud backup")
-
-            }
-
-        }
-
-}
-@Composable
-fun PremiumCard(clicked:Boolean,setIsClicked:(Boolean)-> Unit,changeTextData:()->Unit){
-    val borderColor = if(clicked) MaterialTheme.colors.primary else Color.White
-
-        Card(
-            modifier = Modifier
-
-                .width(160.dp)
-                .clickable {
-                    changeTextData()
-                    setIsClicked(true)
-                },
-            elevation = 5.dp,
-            border = BorderStroke(4.dp, borderColor)
-        ) {
-            Column(
-                modifier = Modifier
-                    .padding(15.dp),
-                horizontalAlignment = Alignment.Start
-            ) {
-                Text("$10.00/month",modifier = Modifier.padding(bottom = 10.dp),style = MaterialTheme.typography.h6)
-                Text("- No calf limit")
-                Text("- Offline usage")
-                Text("- Cloud backup")
-            }
-
-        }
-}
-
-@Composable
-fun DetailTextBox(isPremium:Boolean){
-    val billedText = if(isPremium) "Your Google account will be charged immediately after sign up" else "Your Google account will not be charged. You have selected the free tier"
-    val subText = if(isPremium) "Yes. You can manage your subscription through" else "No. You have selected the free tier"
-    Column(modifier = Modifier.padding(15.dp), horizontalAlignment = Alignment.CenterHorizontally){
-        Text("When will I be billed?",style = MaterialTheme.typography.h6)
-        Column( horizontalAlignment = Alignment.Start){
-            Text(billedText,color = Color.Gray.copy(alpha = .9f))
-        }
-        Text("Does my subscription auto renew?",style = MaterialTheme.typography.h6,modifier = Modifier.padding(top=15.dp))
-        Column( horizontalAlignment = Alignment.CenterHorizontally){
-
-            Text(subText,color = Color.Gray.copy(alpha = .9f))
-            if(isPremium){
-                HighlightedText()
-            }
-
-        }
 
 
-    }
-
-}
 
 
-@Composable
-fun SubmitButton(
-    submit: (Int) -> Unit = {}
-){
-    Button(onClick = {},
-        modifier = Modifier
-            .height(80.dp)
-            .width(280.dp)
-            .padding(start = 0.dp, 20.dp, 0.dp, 0.dp)) {
-
-        Text(text="Register",fontSize = 26.sp)
-    }
-}
-
-@Composable
-fun HighlightedText(
-    modifier:Modifier = Modifier,
-    linkTextColor: Color = Color.Blue,
-    linkTextFontWeight: FontWeight = FontWeight.Medium,
-    linkTextDecoration: TextDecoration = TextDecoration.Underline,
-){
-    val uriHandler = LocalUriHandler.current
-    Text(
-        "Google Play’s Subscription Center",
-        modifier = modifier.clickable{
-            uriHandler.openUri("https://play.google.com/store/account/subscriptions")
-
-        },
-        color = linkTextColor,
-        textDecoration = linkTextDecoration,
-        fontWeight = linkTextFontWeight
-
-
-    )
-
-}
 
