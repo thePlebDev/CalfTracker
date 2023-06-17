e4f7ef83fe3d29250df0c13b95d2bfc546391e3e
Tristan Elliott <>
2023-06-17 19:44:59 -0300

    setting up fakeAuthRepository

---
+++ app/src/test/java/com/elliottsoftware/calftracker/data/repositories/FakeAuthRepository.kt
@@ -0,0 +1,31 @@
+package com.elliottsoftware.calftracker.data.repositories
+
+import com.elliottsoftware.calftracker.domain.models.Response
+import com.elliottsoftware.calftracker.domain.repositories.AuthRepository
+import kotlinx.coroutines.flow.Flow
+
+class FakeAuthRepository:AuthRepository {
+    override suspend fun authRegister(
+        email: String,
+        password: String,
+        username: String
+    ): Flow<Response<Boolean>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun loginUser(email: String, password: String): Flow<Response<Boolean>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun isUserSignedIn(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun signUserOut(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun resetPassword(email: String): Flow<Response<Boolean>> {
+        TODO("Not yet implemented")
+    }
+}
